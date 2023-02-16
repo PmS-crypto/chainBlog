1aa8e61044fb6088a2ac2197b337d828977d4159
Chaitra Shringa <chaitra.shringa@outlook.com>
2023-02-16 22:58:04 +0530

    added uploadImage page to test filecoin upload and deleted component

--- app/pages/uploadImage.js
+++ app/pages/uploadImage.js
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import{ Web3Storage } from 'web3.storage'
 
-function Upload(){
+export default function Upload(){
     const[files,setFile] = useState([])
     const [filename,setfilename] = useState('')
+    const [url,setURL] = useState('')
     const [generatedurl,setGenertedurl] = useState('')
     const [cid,setCid] = useState('')
     const[token,setToken] = useState('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDU1NzIyNzNFMkUyOTJDMzVGMGVCRDgyQjhDZEM1MTE5N0M3ODQ0ODUiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NzYwMTc1NDMxNjQsIm5hbWUiOiJmaXJzdCJ9.rdkBSEp2vZs3W_9mj7eZaDJqjizOwbq3nlsGr-_sUXM')
@@ -18,6 +19,12 @@ function Upload(){
         console.log(generatedurl)
         
     }
+
+    const handleFetchImgClick = async(e) => {
+        e.preventDefault()
+        const fetchimgUrl = `https://${cid}.ipfs.dweb.link/${filename}`
+        setURL(fetchimgUrl)
+    }
     return(
         <form onSubmit={handlesubmit}>
           <input type='file' id='filepicker'  name='fileList' onChange={e => 
@@ -28,7 +35,9 @@ function Upload(){
                     }
                     } multiple required />
             <button>Uploadimage</button>
+        
+            <button onClick={handleFetchImgClick}>Fetch Image</button>
+            <img src={url} alt="txt" />
         </form>
     )
 }
-export default Upload
